67d9affde6c4dba5349d1dacdbc3f3fada36ac18
Breno Viana <brenomfviana@gmail.com>
2020-05-24 01:34:46 -0300

    removes target folder

--- pool/target/debug/deps/pool-712914b175fb020b.d
+++
@@ -1,5 +0,0 @@
-/home/breno/Documents/Workspace/Projects/rust-web-server/pool/target/debug/deps/pool-712914b175fb020b: src/lib.rs
-
-/home/breno/Documents/Workspace/Projects/rust-web-server/pool/target/debug/deps/pool-712914b175fb020b.d: src/lib.rs
-
-src/lib.rs:
--- pool/target/debug/pool-712914b175fb020b.d
+++
@@ -1,1 +0,0 @@
-/home/breno/Documents/Workspace/Projects/rust-web-server/pool/target/debug/pool-712914b175fb020b: /home/breno/Documents/Workspace/Projects/rust-web-server/pool/src/lib.rs
