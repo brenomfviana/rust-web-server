db1df5e930d81e70a47a5123a8728c8daf6bbacb
Breno Viana <brenomfviana@gmail.com>
2020-05-24 00:21:21 -0300

    adds 200 and 404 response and HTML pages

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     for stream in listener.incoming() {
       // handle the connection of each stream
       if let Ok(stream) = stream { handle_connection(stream); }
+      else { println!("WARNNIG: The connection could not be established."); }
     }
   } else { panic!("The server is not allowed to use this IP and Port."); }
 }
@@ -21,11 +22,33 @@ fn handle_connection(mut stream: TcpStream) {
   // Read request
   let mut buffer = [0; 512];
   stream.read(&mut buffer).unwrap();
-  // Read HTML page
-  let contents = fs::read_to_string("hello.html").unwrap();
-  // Prepare response
-  let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+  // Define the GET methods
+  let get = b"GET / HTTP/1.1\r\n";
+  // Get the HTTP header and HTML filename
+  let (status_line, filename) = if buffer.starts_with(get) {
+    ("HTTP/1.1 200 OK\r\n\r\n", "pages/index.html")
+  } else {
+    ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "pages/404.html")
+  };
+  // Server response
+  let response: String;
+  // Read HTML content
+  if let Ok(contents) = fs::read_to_string(filename) {
+    // Prepare HTML response
+    response = format!("{}{}", status_line, contents);
+  } else {
+    // Internal server error
+    let status_line = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+    // Prepare HTML response
+    response = format!("{}", status_line);
+  }
   // Return HTML response
-  stream.write(response.as_bytes()).unwrap();
-  stream.flush().unwrap();
+  if let Err(_) = stream.write(response.as_bytes()) {
+    println!("The server could not write the HTML response.");
+  } else {
+    // Check if the server sent the response to the client
+    if let Err(_) = stream.flush() {
+      println!("The server could not response the client.");
+    } else { println!("Everything it's OK!"); }
+  }
 }
